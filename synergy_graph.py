81a72bb75969cd9e666352f725c153d6cbfad9f3
Jormii <lopeznatalj@gmail.com>
2023-02-02 20:14:45 +0100

    Fixed predicate addition

--- synergy_graph.py
+++ synergy_graph.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Dict, Set
+from typing import Dict, List, Set
 
 from action import Action
 from subject import Subject
@@ -37,22 +37,26 @@ class SynergyGraph:
     def __init__(self) -> None:
         self.subjects: Dict[Subject, SynergyGraph.Node] = {}
 
-    def add_predicate(self, predicate: IPredicate) -> None:
-        # Head
-        head = predicate.head()
-        self._get_subject_node(head).add_output(predicate)
+    def subject_present(self, subject: Subject) -> bool:
+        return subject in self.subjects
 
-        # Tails
+    def get_subject_node(self, subject: Subject) -> SynergyGraph.Node:
+        return self.subjects[subject]
+
+    def add_predicate(self, predicate: IPredicate) -> None:
         for subpredicate in predicate.unwrap():
+            head = subpredicate.head()
             tail = subpredicate.tail()
             action = subpredicate.action()
-            self._get_subject_node(tail).add_input(action)
 
-    def _get_subject_node(self, subject: Subject) -> SynergyGraph.Node:
-        if subject not in self.subjects:
+            self._get_or_create_subject_node(tail).add_input(action)
+            self._get_or_create_subject_node(head).add_output(predicate)
+
+    def _get_or_create_subject_node(self, subject: Subject) -> SynergyGraph.Node:
+        if not self.subject_present(subject):
             self.subjects[subject] = SynergyGraph.Node()
 
-        return self.subjects[subject]
+        return self.get_subject_node(subject)
 
     def __repr__(self) -> str:
         s = ""
